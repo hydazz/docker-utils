025818e975731386e4490e8c0a047d84c98288db
hydazz <alexanderhyde@icloud.com>
2021-02-16 10:55:34 +1100

    Update docker-ci.sh

--- docker/docker-ci.sh
+++ docker/docker-ci.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # watches docker logs for '${TEST_SEARCH}' which usally means container booted successfully
 # this script is not really useful as if a service fails to start it will not know
-
+set -x
 TIMEOUT=$((SECONDS + 120)) # time in seconds to wait for '${TEST_SEARCH}' in logs (timeout)
 
-docker run -d --name="ci-test" -e DEBUG=true vcxpz/ci-build:ci-build
+docker run -d --name="ci-test" -e DEBUG=true vcxpz/ci-build:ci-build >/dev/null
 while [[ "$SECONDS" -lt "$TIMEOUT" ]]; do
 	if docker logs "ci-test" 2>&1 | grep -q "${TEST_SEARCH}"; then
 		FAILED=false
